9528e167026dac23435ae2d726ad1f2ab177745b
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-08-31 21:00:31 +0630

    add favoriteBlog function

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,17 @@ const dummy = blogs => {
 }
 
 const totalLikes = blogs => {
-    return blogs.reduce((pval, cval) => pval+cval.likes, 0)
+    return blogs.reduce((pval, cval) => pval + cval.likes, 0)
+}
+
+const favoriteBlog = blogs => {
+    const blog = blogs.reduce((pval, cval) => pval.likes > cval.likes ? pval : cval, {})
+
+    return {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+    }
 }
 
 module.exports = {
