c11c6576cffad632ff767ee931c82a766cb83ee5
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-09-12 21:03:08 +0630

    declare route variable and refractor code

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,6 +5,7 @@ const Blog = require('../models/blog')
 const helper = require('./test_helper')
 
 const api = supertest(app)
+const route = '/api/blogs'
 
 beforeEach(async () => {
     Blog.deleteMany({})
@@ -16,18 +17,30 @@ beforeEach(async () => {
 describe('blog api test', () => {
     test('specific blog is within the returned blogs', async () => {
         await api
-            .get('/api/blogs')
+            .get(route)
             .expect(200)
             .expect('Content-Type', /application\/json/)
     })
 
     test('return blog object to contain id property', async () => {
-        const blogs = await api.get('/api/blogs')
+        const blogs = await api.get(route)
 
         blogs.body.map(blog => {
             expect(blog).toHaveProperty('id')
         })
     })
+
+    // test('a valid blog is added',async () => {
+    //     const newBlog = {
+    //         title: "Canonical string reduction",
+    //         author: "Edsger W. Dijkstra",
+    //         url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    //         likes: 12,
+    //     }
+
+    //     const blogs = await api
+    //     .post('/api/')
+    // })
 })
 
 afterAll(() => {
