c3231aa0bce5e59de0e2d24394be946076ffc6f1
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-09-12 21:26:53 +0630

    modify db connection to test db

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 
 const PORT=process.env.PORT
 const MONGODB_URI=process.env.NODE_ENV === 'test' 
-    ?process.env.MONGODB_URI
-    :process.env.TEST_MONGODB_URI
+    ?process.env.TEST_MONGODB_URI
+    :process.env.MONGODB_URI
 
 module.exports={
     PORT,
