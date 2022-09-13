42fabe8b1958cc2f9d35a24b2d0cb220d2e24e15
MyatKaungKyaw <myatkaungg31@gmail.com>
2022-09-13 21:00:05 +0630

    refractor route  variable to endPoint

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -5,7 +5,7 @@ const Blog = require('../models/blog')
 const helper = require('./test_helper')
 
 const api = supertest(app)
-const route = '/api/blogs'
+const endPoint = '/api/blogs'
 
 beforeEach(async () => {
     await Blog.deleteMany({})
@@ -17,13 +17,13 @@ beforeEach(async () => {
 describe('blog api test', () => {
     test('specific blog is within the returned blogs', async () => {
         await api
-            .get(route)
+            .get(endPoint)
             .expect(200)
             .expect('Content-Type', /application\/json/)
     })
 
     test('return blog object to contain id property', async () => {
-        const blogs = await api.get(route)
+        const blogs = await api.get(endPoint)
 
         blogs.body.map(blog => {
             expect(blog).toHaveProperty('id')
@@ -39,19 +39,19 @@ describe('blog api test', () => {
         }
 
         await api
-            .post(route)
+            .post(endPoint)
             .send(newBlog)
             .expect(201)
-            .expect('Content-Type',/application\/json/)
+            .expect('Content-Type', /application\/json/)
 
-        const blogs = await api.get(route)
+        const blogs = await api.get(endPoint)
         const titles = blogs.body.map(blog => blog.title)
 
         expect(blogs.body).toHaveLength(helper.initialBlog.length + 1)
         expect(titles).toContain('Canonical string reduction')
     })
 
-    test('blog with no like property request is default to 0 value',async () => {
+    test('blog with no like property request is default to 0 value', async () => {
         const blogWitNoLike = {
             title: "Canonical string reduction",
             author: "Edsger W. Dijkstra",
@@ -59,16 +59,32 @@ describe('blog api test', () => {
         }
 
         await api
-            .post(route)
+            .post(endPoint)
             .send(blogWitNoLike)
             .expect(201)
-            .expect('Content-Type',/application\/json/)
+            .expect('Content-Type', /application\/json/)
 
-        const blogs = await api.get(route)
+        const blogs = await api.get(endPoint)
         const blog = blogs.body.find(blog => blog.title === 'Canonical string reduction')
         console.log(blog)
         expect(blog.likes).toEqual(0)
     })
+
+    // test('blog with no title or url is not added', async  => {
+    //     const blogWitNoTitle = {
+    //         author: "Edsger W. Dijkstra",
+    //         url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    //         likes:5,
+    //     }
+
+    //     const blogWitNoURL = {
+    //         title: "useId Hook Explained",
+    //         author: "Ahsan Ali Mansoor",
+    //         likes: 4,
+    //     }
+
+    //     await api.post()
+    // })
 })
 
 afterAll(() => {
