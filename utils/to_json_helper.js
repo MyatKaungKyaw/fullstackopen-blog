9fff3d5dc329474465e63b57419f84bcb3c64535
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-26 07:39:21 +0000

    change passwordHash to password in transformForUser function

--- utils/to_json_helper.js
+++ utils/to_json_helper.js
@@ -2,7 +2,7 @@ const transformForUser = (document,returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    delete returnedObject.passwordHash
+    delete returnedObject.password
 }
 
 const transformNormal = (document,returnedObject) => {
