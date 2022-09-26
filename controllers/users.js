c5b2c9b173d3a0c9b521454544686bbb4f9cbf9e
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-26 10:50:14 +0630

    save user in post request

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,22 @@
-const usersRouter = require('mongoose.Router')
+const usersRouter = require('mongoose').Router()
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
 
-usersRouter.post('/',async (req,res) => {
+usersRouter.post('/', async (req, res) => {
+    const { username, name, password } = req.body
 
+    const saltRounds = 10
+    const passwordHash = bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username: username,
+        name: name,
+        password: passwordHash
+    })
+    
+    const savedUser = await user.save()
+
+    res.status(201).json(savedUser)
 })
+
+module.exports = usersRouter
