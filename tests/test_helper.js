0fac66541a0b99c161f3cb9ea2d03ab9d8c926d2
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-26 10:58:15 +0630

    add usersInDb function

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 const initialBlog = [
     {
@@ -20,8 +21,14 @@ const blogsInDb = async () => {
     return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+    const users = await User.find({})
+    return users.map(user => user.toJSON())
+}
+
 module.exports = {
     initialBlog,
     blogsInDb,
-
+    userInDb,
+    
 }
