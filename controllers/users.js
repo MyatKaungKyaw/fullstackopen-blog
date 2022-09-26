72092dec221f9629ec42bf815940ad64f658f7fa
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-26 17:07:27 +0630

    validate user input

--- controllers/users.js
+++ controllers/users.js
@@ -5,6 +5,33 @@ const bcrypt = require('bcryptjs')
 usersRouter.post('/', async (req, res) => {
     const { username, name, password } = req.body
 
+    const checkUser = await User.find({username})
+
+    // validate username
+    if (!username) {
+        res.status(400).json({
+            error: 'username missing'
+        })
+    } else if (username.length < 3) {
+        res.status(400).json({
+            error: 'username length must be at least 3 characters long'
+        })
+    }else if (username === checkUser.username) {
+        res.status(400).json({
+            error: 'username must be unique'
+        })
+    }// validate password 
+    else if (!password) {
+        res.status(400).json({
+            error: 'password missing'
+        })
+    }else if(password.length <3){
+        res.status(400).json({
+            error:'password length must be at least 3 characters long'
+        })
+    }
+
+    //hash password
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(password, saltRounds)
 
@@ -13,7 +40,7 @@ usersRouter.post('/', async (req, res) => {
         name: name,
         password: passwordHash
     })
-    
+
     const savedUser = await user.save()
 
     res.status(201).json(savedUser)
