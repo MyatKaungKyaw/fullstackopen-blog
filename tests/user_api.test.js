cc39508ff99913d0daa50e6ac1a9a5bedc74b56f
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-26 13:20:10 +0630

    Create test for user api

---
+++ tests/user_api.test.js
@@ -0,0 +1,42 @@
+const User = require('../models/user')
+const helper = require('./test_helper')
+const bcrypt = require('bcryptjs')
+const app = requier('../app')
+
+const api = supertest(app)
+
+describe('when there is user data in database',() =>{
+    beforeEach(async () =>{
+        await User.deleteMany({})
+        const passwordHash = await bcrypt.hash('sekret',10)
+        const user = new User({
+            username: 'root',
+            name: 'origin root',
+            password: passwordHash
+        })
+
+        await user.save()
+    })
+
+    test('creation success with new user name',async () => {
+        const usersAtStart = await helper.usersInDb()
+        const passwordHash = await bcrypt.hash('robin',10)
+        const newUser = new User({
+            username: 'robin',
+            name: 'robin hood',
+            password: passwordHash
+        })
+
+        await api
+        .post('/api/users')
+        .send(newUser)
+        .expect(201)
+        .expect('Content-Type',/application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length+1)
+        
+        const usernames = usersAtEnd.map(user => user.username)
+        expect(usernames).toContain(newUser.username)
+    })
+})
