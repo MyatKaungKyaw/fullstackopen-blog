93dc9dabd9b6031be778a34456786ae58d615af3
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-27 02:25:37 +0000

    check checkUser length before comparing it's username with username

--- controllers/users.js
+++ controllers/users.js
@@ -3,55 +3,53 @@ const User = require('../models/user')
 const bcrypt = require('bcryptjs')
 
 usersRouter.post('/', async (req, res) => {
-    const { username, name, password } = req.body
+  const { username, name, password } = req.body
 
-    const checkUser = await User.find({ username })
+  const checkUser = await User.find({ username })
 
-    console.log(username)
-
-    // validate username
-    if (!username) {
-        res.status(400).json({
-            error: 'username missing'
-        })
-    } else if (username.length < 3) {
-        res.status(400).json({
-            error: 'username length must be at least 3 characters long'
-        })
-    } else if (username === checkUser[0].username) {
-        res.status(400).json({
-            error: 'username must be unique'
-        })
-    }// validate password 
-    else if (!password) {
-        res.status(400).json({
-            error: 'password missing'
-        })
-    } else if (password.length < 3) {
-        res.status(400).json({
-            error: 'password length must be at least 3 characters long'
-        })
-    }
-
-    console.log('1')
-
-    //hash password
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
-
-
-    console.log('2')
-    const user = new User({
-        username: username,
-        name: name,
-        password: passwordHash
+  // validate username
+  if (!username) {
+    res.status(400).json({
+      error: 'username missing'
     })
+  } else if (username.length < 3) {
+    res.status(400).json({
+      error: 'username length must be at least 3 characters long'
+    })
+  } else if (checkUser.length > 1) {
+    if (username === checkUser[0].username) {
+      res.status(400).json({
+        error: 'username must be unique'
+      })
+    }
+  }// validate password 
+  else if (!password) {
+    res.status(400).json({
+      error: 'password missing'
+    })
+  } else if (password.length < 3) {
+    res.status(400).json({
+      error: 'password length must be at least 3 characters long'
+    })
+  }
 
-    console.log('3')
+  //hash password
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    const savedUser = await user.save()
+  const user = new User({
+    username: username,
+    name: name,
+    password: passwordHash
+  })
 
-    res.status(201).json(savedUser)
+  const savedUser = await user.save()
+  res.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async (req, res) => {
+  const allUsers = await User.find({})
+  res.json(allUsers)
 })
 
 module.exports = usersRouter
