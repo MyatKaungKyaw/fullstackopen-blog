c9063cc981e0043322059cb4c08e770319b91e9a
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-27 10:28:25 +0630

    add first user in user schema to user field while save in post and populate users in get

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,46 +1,53 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog
+        .find({})
+        .populate('users')
     response.json(blogs)
 })
 
 blogsRouter.post('/', async (request, response) => {
-    const body=request.body
-    if(!body.title || !body.url) {
+    const body = request.body
+    if (!body.title || !body.url) {
         return response.status(400).end()
     }
 
+    const allUsers = await User.find({})
+    const userId = allUsers.length > 0 ? allUsers[0]._id : '';
+
     const blog = new Blog({
-        title:body.title,
-        author:body.author,
-        url:body.url,
-        likes:body.likes || 0,
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+        user: userId,
     })
 
     const result = await blog.save()
     response.status(201).json(result)
 })
 
-blogsRouter.delete('/:id',async (request,response) => {
+blogsRouter.delete('/:id', async (request, response) => {
     await Blog.findByIdAndDelete(request.params.id)
     response.status(204).end()
 })
 
-blogsRouter.put('/:id',async (request,response) => {
-  const id = request.params.id
-  const body = request.body
-  const blog = {
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes,
-  }
-  console.log('before upd')
-  const updBlog = await Blog.findByIdAndUpdate(id,blog,{new:true})
-  console.log('after upd')
-  response.status(201).json(updBlog)
+blogsRouter.put('/:id', async (request, response) => {
+    const id = request.params.id
+    const body = request.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+    console.log('before upd')
+    const updBlog = await Blog.findByIdAndUpdate(id, blog, { new: true })
+    console.log('after upd')
+    response.status(201).json(updBlog)
 })
 
 module.exports = blogsRouter
