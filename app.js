7df1d5e21288c236252ec2304f2cbab866ccb4b2
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-28 15:34:02 +0630

    add tokenExtractor in route

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.tokenExtractor)
 app.use(middleware.requestLogger)
 app.use('/api/blogs',blogRouter)
 app.use('/api/users',userRouter)
