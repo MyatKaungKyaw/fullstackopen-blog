ff91cc98ec71ea2f256ebb4e6ceeb5615a92aa99
Lu Mike <33856024+MyatKaungKyaw@users.noreply.github.com>
2022-09-28 06:16:00 +0000

    fix bug in login

--- controllers/logIn.js
+++ controllers/logIn.js
@@ -1,17 +1,18 @@
-const jwt = require('jasonwebtoken')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const User = require('../models/user')
-const user = require('../models/user')
 const loginRouter = require('express').Router()
 
 loginRouter.post('/', async (req, res) => {
     const { username, password } = req.body
 
-    const user = await user.find({ username })
+    const user = await User.findOne({ username })
+
+    console.log(user)
 
     const passwordCorrect = user === null
         ? false
-        //compare password from request and from database
+        //compare password from request and hash
         : await bcrypt.compare(password, user.password)
 
     if (!(user && passwordCorrect)) {
@@ -26,8 +27,8 @@ loginRouter.post('/', async (req, res) => {
     //create token with time expiration
     const token = jwt.sign(
         userForToken,
-        process.env.SECRET
-        , { expireIn: 60 * 60 }
+        process.env.SECRET,
+        { expiresIn: 60 * 60 }
     )
 
     res
